4101b7b00b415fb00ab4931e0700bc540bdf239c
juyeon <juyeonh@rainist.com>
2020-07-04 12:54:07 +0900

    Add router for creating tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,32 @@
 <script>
+  import { Router, Link, Route } from 'svelte-routing';
+
+  export let url = '';
 </script>
 
 <main>
+  <Router url='{ url }'>
+    <nav>
+      <Link to='/yesterday'>어제</Link>
+      <Link to='/'>오늘</Link>
+      <Link to='/tomorrow'>내일</Link>
+      <Link to='/week'>일주일</Link>
+    </nav>
+    <div>
+      <Route path='/yesterday'>
+        <div>어제</div>
+      </Route>
+      <Route path='/tomorrow'>
+        <div>내일</div>
+      </Route>
+      <Route path='/week'>
+        <div>일주일</div>
+      </Route>
+      <Route path='/'>
+        <div>오늘</div>
+      </Route>
+    </div>
+  </Router>
 </main>
 
 <style>
