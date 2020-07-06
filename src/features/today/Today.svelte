871c593b289f31879a03b9f99260bf244d7ecba4
juyeon <juyeonh@rainist.com>
2020-07-06 21:25:43 +0900

    Change feature component name to Today

---
+++ src/features/today/Today.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Container from '../../components/Container.svelte';
+  export let current;
+  export let hourly;
+  console.log('hourly', hourly);
+  console.log('current', current);
+</script>
+
+<Container>
+  <div>Weather</div>
+</Container>
