6bee03a2cb19ea98d54f4061469b5f2df7a2bce7
juyeon <juyeonh@rainist.com>
2020-07-06 16:13:40 +0900

    Print address for header

--- src/features/app/Header.svelte
+++ src/features/app/Header.svelte
@@ -2,9 +2,8 @@
   import { get } from 'svelte/store';
   import { onDestroy } from 'svelte';
   import geolocationStore from '../../store/geolocation.js';
+  import HeaderTop from '../../components/HeaderTop.svelte';
   import { getAddressByGeolocation } from '../../api';
-
-  getAddressByGeolocation(37.5665, 126.9780);
 
   const geolocationOptions = {
     timeout: 10000,
@@ -12,7 +11,13 @@
     maximumAge: 600000
   };
 
-  const handleSuccess = ({ coords }) => {
+  const DEFAULT_ADDRESS = {
+    region_1depth_name: '서울',
+    region_2depth_name: '중구',
+    region_3depth_name: '태평로1가',
+  };
+
+  const handleSuccess = async ({ coords }) => {
     let { location } = get(geolocationStore);
 
     const isSameLocation = coords.latitude === location.latitude
@@ -22,11 +27,24 @@
       return;
     }
 
+    const {
+      documents,
+      meta: { total_count }
+    } = await getAddressByGeolocation(
+      coords.latitude,
+      coords.longitude
+    );
+
+    const address = total_count > 0
+      ? (documents[0].road_address || documents[0].address)
+      : DEFAULT_ADDRESS;
+
     geolocationStore.update(prevStore => ({
       fetchState: 'DONE',
       location: {
         latitude: coords.latitude,
-        longitude: coords.longitude
+        longitude: coords.longitude,
+        address,
       }
     }));
   };
@@ -34,7 +52,12 @@
   const handleError = (error) => {
     geolocationStore.update(prevStore => ({
       ...prevStore,
-      fetchState: 'ERROR'
+      fetchState: 'ERROR',
+      location: {
+        latitude: 37.5665,
+        longitude: 126.9780,
+        address: DEFAULT_ADDRESS
+      }
     }));
   };
 
@@ -46,11 +69,15 @@
 </script>
 
 <header>
-  {#if $geolocationStore.fetchState === 'DONE'}
-    <h1>날씨</h1>
-  {:else if $geolocationStore.fetchState === 'ERROR'}
-    <h1>에러</h1>
-  {:else}
+  {#if $geolocationStore.fetchState === 'LOADING'}
     <h1>로딩중</h1>
+  {:else}
+    <HeaderTop
+      title={$geolocationStore.location.address.region_1depth_name}
+      subtitle={`
+        ${$geolocationStore.location.address.region_2depth_name} 
+        ${$geolocationStore.location.address.region_3depth_name}
+      `}
+    />
   {/if}
 </header>
