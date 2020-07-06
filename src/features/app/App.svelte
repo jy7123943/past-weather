2dfeb5a565d73a5a7ba562944dcf20017852f52e
juyeon <juyeonh@rainist.com>
2020-07-06 17:09:46 +0900

    Add loader for app feature

--- src/features/app/App.svelte
+++ src/features/app/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { Router, Link, Route } from 'svelte-routing';
+  import geolocationStore from '../../store/geolocation.js';
   import Navigator from './Navigator.svelte';
   import Header from './Header.svelte';
   import Weather from '../weather/Weather.svelte';
@@ -9,21 +10,23 @@
 
 <main>
   <Header />
-  <Router url='{ url }'>
-    <div>
-      <Route path='/yesterday'>
-        <div>어제</div>
-      </Route>
-      <Route path='/tomorrow'>
-        <div>내일</div>
-      </Route>
-      <Route path='/week'>
-        <div>일주일</div>
-      </Route>
-      <Route path='/' component={ Weather } />
-    </div>
-    <Navigator />
-  </Router>
+  {#if $geolocationStore.fetchState !== 'LOADING'}
+    <Router url='{ url }'>
+      <div>
+        <Route path='/yesterday'>
+          <div>어제</div>
+        </Route>
+        <Route path='/tomorrow'>
+          <div>내일</div>
+        </Route>
+        <Route path='/week'>
+          <div>일주일</div>
+        </Route>
+        <Route path='/' component={ Weather } />
+      </div>
+      <Navigator />
+    </Router>
+  {/if}
 </main>
 
 <style>
