a963af5c4b98d116297bf1cf9e453ee29a65504e
juyeon <juyeonh@rainist.com>
2020-07-12 13:42:29 +0900

    Change Today component

--- src/features/today/Today.svelte
+++ src/features/today/Today.svelte
@@ -1,31 +1,24 @@
 <script>
-  import moment from 'moment-timezone';
+  import { convertDateToArray, getIcon } from '../../util';
   import Container from '../../components/Container.svelte';
+  import WeatherCard from '../../components/WeatherCard.svelte';
+
   export let current;
   export let hourly;
-  export let timezone = 'Asia/Seoul';
-  console.log('hourly', hourly);
-  console.log('current', current);
+  export let day;
+  export let timezone;
 
-  const convertDate = (dt) => {
-    return moment(dt * 1000).tz(timezone).format('YYYY MM DD HH:mm');
+  const weatherData = {
+    ...current,
+    timezone,
+    day: day.temp,
+    rain: current.rain ? current.rain['1h'] : day.rain,
+    snow: current.snow ? current.snow['1h'] : day.snow
   };
-// clouds: 75
-// dew_point: 17.58
-// dt: 1594038688
-// feels_like: 26.8
-// humidity: 61
-// pressure: 1000
-// sunrise: 1593980210
-// sunset: 1594032987
-// temp: 25.66
-// uvi: 9.15
-// visibility: 10000
-// weather: [{id: 803, main: "Clouds", description: "튼구름", icon: "04n"}]
-// wind_deg: 269
-// wind_speed: 2.1
+  console.log('current', current);
+  console.log('day', day);
 </script>
 
 <Container>
-  <div>{convertDate(current.dt)}</div>
+  <WeatherCard {weatherData} />
 </Container>
