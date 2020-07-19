29ac99f4b9c98b3005f31d7186deed2825337be0
juyeon <juyeonh@rainist.com>
2020-07-19 19:02:21 +0900

    Add util for get day

--- src/util/index.js
+++ src/util/index.js
@@ -1,5 +1,11 @@
 import moment from 'moment-timezone';
 
+moment.tz.setDefault('Asia/Seoul');
+moment.locale('ko', {
+  weekdays : '일요일_월요일_화요일_수요일_목요일_금요일_일요일'.split('_'),
+  weekdaysShort : '일_월_화_수_목_금_토'.split('_'),
+});
+
 export const convertDateToArray = (dt, timezone) => (
   moment(dt * 1000).tz(timezone).format('MM/DD h:mma').split(' ')
 );
@@ -10,6 +16,10 @@ export const convertDateToHour = (dt, timezone) => (
 
 export const convertDate = (dt, timezone) => (
   moment(dt * 1000).tz(timezone).format('M/D')
+);
+
+export const convertDay = (dt, timezone) => (
+  moment(dt * 1000).locale('ko').tz(timezone).format('ddd')
 );
 
 export const getIcon = (icon) => `http://openweathermap.org/img/wn/${icon}@2x.png`;
